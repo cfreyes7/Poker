7123872f49d1d5241fdc01c5bbb4e2fa3cc4c95b
Fabian Reyes <cfreyes7@gmail.com>
2020-06-29 14:05:28 -0500

    initial commit

---
+++ Poker/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class PokerStraigth
+    {
+        static void Main(string[] args)
+        {
+            List<int> cards = new List<int> { 2, 3, 4, 5, 6 };
+            Console.WriteLine("{2, 3, 4 ,5, 6} Es escalera: " + isStraight(cards));
+
+            cards = new List<int> { 14, 5, 4, 2, 3 };
+            Console.WriteLine("{14, 5, 4 ,2, 3} Es escalera: " + isStraight(cards));
+
+            cards = new List<int> { 7, 7, 12, 11, 3, 4, 14 };
+            Console.WriteLine("{7, 7, 12 ,11, 3, 4, 14} Es escalera: " + isStraight(cards));
+
+            cards = new List<int> { 7, 3, 2 };
+            Console.WriteLine("{7, 3, 2} Es escalera: " + isStraight(cards));
+
+            Console.ReadLine();
+        }
+
+        public static bool isStraight(List<int> cards)
+        {
+
+            bool respuesta = false;
+
+            int last = 0;
+            int count = 0;
+            int valor = cards.Count() >= 5 ? 5 : cards.Count();
+
+            cards.Sort();
+
+            if (cards.Contains(14))
+            {
+                cards.Add(1);
+                cards.Sort();
+                valor = valor - 1;
+            }
+
+
+            foreach (int card in cards)
+            {
+                if (card.Equals(last)) {
+                    continue;
+                }
+                else if (card.Equals(++last)) {
+                    count++;
+                    last = card;
+                }
+                else
+                {
+                    count = 1;
+                    last = card;
+                }
+
+                if (count.Equals(valor))
+                {
+                    respuesta = true;
+                }
+
+            }
+
+            return respuesta;
+
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> cards = new List<int>{ 2, 3 ,4 ,5 ,6 };
+           
+            var results = Poker.PokerStraigth.isStraight(cards);
+            Assert.AreEqual(results, true, "2, 3 ,4 ,5 ,6");
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<int> cards = new List<int> { 14, 5, 4, 2, 3 };
+
+            var results = Poker.PokerStraigth.isStraight(cards);
+            Assert.AreEqual(results, true, "14, 5, 4 ,2, 3");
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            List<int> cards = new List<int> { 7, 7, 12 ,11, 3, 4, 14 };
+
+            var results = Poker.PokerStraigth.isStraight(cards);
+            Assert.AreEqual(results, false, "7, 7, 12 ,11, 3, 4, 14");
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            List<int> cards = new List<int> { 7, 3, 2 };
+
+            var results = Poker.PokerStraigth.isStraight(cards);
+            Assert.AreEqual(results, false, "7, 3, 2");
+
+        }
+        
+    }
+}
